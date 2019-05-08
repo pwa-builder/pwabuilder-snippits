017295c336c87cfef74db290d48b2a7799a9b6ab
Jeff Burtoft <boyofgreen@gmail.com>
2019-05-07 22:35:13 -0700

    Update installButton.js

--- src/installButton/installButton.js
+++ src/installButton/installButton.js
@@ -1,7 +1,7 @@
-let deferredPrompt = null;
+
+    let deferredPrompt = null;
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  console.log(e);
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
@@ -11,15 +11,19 @@ window.addEventListener('beforeinstallprompt', (e) => {
 async function install() {
   if (deferredPrompt) {
     deferredPrompt.prompt();
+    console.log(deferredPrompt)
+    deferredPrompt.userChoice.then(function(choiceResult){
 
-    const choiceResult = await deferredPrompt.userChoice();
-
-    if (choiceResult.outcome === 'accepted') {
+      if (choiceResult.outcome === 'accepted') {
       console.log('Your PWA has been installed');
     } else {
       console.log('User chose to not install your PWA');
     }
 
     deferredPrompt = null;
+
+    });
+
+ 
   }
 }
